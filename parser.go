7d246e547d1be5ebe8c9c4cea1868b661e5428ab
kraser <kravalsergey@yandex.ru>
2019-02-14 09:20:10 +0700

    Multystream

--- parser.go
+++ parser.go
@@ -5,12 +5,11 @@ import (
 	errs "errorshandler"
 	"webreader"
 
-	//"fmt"
 	log "logger"
 	"math/rand"
 
-	//"os"
-	//"os/signal"
+	"os"
+	"os/signal"
 	"priceloader"
 	"time"
 )
@@ -21,9 +20,12 @@ const (
 
 type InterfaceCustomParser interface {
 	ParseCategories(param string)
-	ParseItems()
+	CheckCategoriesTree(map[string]*priceloader.Category, int)
+	CreateItemsUrl(url string) string
+	ParseItems(pCategory *priceloader.Category, param string)
 	ParserInit(*ParserObject)
 	ParserRun()
+	//HandleError()
 }
 
 type ParserOptions struct {
@@ -31,6 +33,7 @@ type ParserOptions struct {
 	URL            string
 	Loaders        int
 	LoaderCapacity int
+	Trials         int
 }
 
 type ParserObject struct {
@@ -44,73 +47,81 @@ func (pParser *ParserObject) init() {
 	priceloader.PriceList.PriceList(pParser.CustomParserOptions.Name)
 	rand.Seed(time.Now().UnixNano())
 	pParser.Options = webreader.GetOptions()
-	pParser.SetUserAgent(useragents[rand.Intn(len(useragents))])
+	pParser.Options.SetRandUserAgent()
 
 	pParser.Options.Url = pParser.CustomParserOptions.URL
 	pParser.CustomParserActions.ParserInit(pParser)
 	log.Info("PARSER_INIT_DONE")
 }
 
-func (pParser *ParserObject) SetUserAgent(ua string) {
-	log.Debug("UA:", ua)
-	pParser.Options.UserAgent = ua
-}
-
 func (pParser *ParserObject) Run() {
 	pParser.init()
-	/*
-		//Подготовим каналы и регулятор
-		taskChan := make(chan priceloader.LoadTask)
-		quitChan := make(chan bool)
-		pController := &LoadController{Loaders: pParser.CustomParserOptions.Loaders, LoaderCapacity: pParser.CustomParserOptions.LoaderCapacity}
-		pController.init(taskChan)
-
-		//Приготовимся перехватывать сигнал останова в канал keys
-		keys := make(chan os.Signal, 1)
-		signal.Notify(keys, os.Interrupt)
-
-		//go pController.balance(quitChan)
-		//go pParser.taskGenerator(taskChan)
-
-		log.Info("MAIN_CYCLE_START")
-		//Основной цикл программы:
-		for {
-			select {
-			case <-keys: //пришла информация от нотификатора сигналов:
-				log.Info("CTRL-C: Ожидаю завершения активных загрузок")
-				quitChan <- true //посылаем сигнал останова балансировщику
-
-			case <-quitChan: //пришло подтверждение о завершении от балансировщика
-				log.Info("MAIN_CYCLE_END")
-				return
-			}
-		}
-	*/
 
 	result, err := webreader.DoRequest(pParser.CustomParserOptions.URL, pParser.Options)
 	errs.ErrorHandle(err)
 	log.CheckHtml(pParser.CustomParserOptions.URL, result, "debug")
 	pParser.CustomParserActions.ParseCategories(result)
+	pParser.CustomParserActions.CheckCategoriesTree(priceloader.PriceList.Categories, 0)
 
-	pParser.CustomParserActions.ParseItems()
+	//Подготовим каналы и регулятор
+	taskChan := make(chan priceloader.LoadTask)
+	quitChan := make(chan bool)
+	pController := &LoadController{Loaders: pParser.CustomParserOptions.Loaders, LoaderCapacity: pParser.CustomParserOptions.LoaderCapacity}
+	pController.init(taskChan)
+	log.Info(len(priceloader.PriceList.ItemsCategories))
+
+	//Приготовимся перехватывать сигнал останова в канал keys
+	keys := make(chan os.Signal, 1)
+	signal.Notify(keys, os.Interrupt)
+
+	go pController.balance(quitChan)
+	go pParser.taskGenerator(taskChan)
+
+	log.Info("MAIN_CYCLE_START")
+	//Основной цикл программы:
+	for {
+		select {
+		case <-keys: //пришла информация от нотификатора сигналов:
+			log.Info("CTRL-C: Ожидаю завершения активных загрузок")
+			quitChan <- true //посылаем сигнал останова балансировщику
+
+		case <-quitChan: //пришло подтверждение о завершении от балансировщика
+			log.Info("MAIN_CYCLE_END")
+			log.Info(len(priceloader.PriceList.ItemsCategories))
+			for _, pCategory := range priceloader.PriceList.ItemsCategories {
+				log.Info(pCategory.Name, pCategory.URL)
+			}
+			return
+		}
+	}
+
 }
 
-/*
 func (pParser *ParserObject) taskGenerator(out chan priceloader.LoadTask) {
 	pPriceList := priceloader.PriceList
-	for _, value := range pPriceList.Categories {
-		log.Info(value.Name)
-		for _, subCat := range value.Categories {
-			subCat.URL = URL + subCat.URL
-			log.Info("  ", subCat.Name, subCat.URL)
-			task := priceloader.LoadTask{Pointer: subCat, Message: "TASK"}
+	var toLoad bool
+	toLoad = true
+	for toLoad {
+		for _, category := range pPriceList.ItemsCategories {
+			task := priceloader.LoadTask{Pointer: category, Handler: pParser.LoadItems, Message: "TASK"}
 			out <- task
 		}
+		toLoad = len(pPriceList.ItemsCategories) > 0
 	}
 	endTask := priceloader.LoadTask{Pointer: nil, Message: ENDMESSAGE}
 	out <- endTask
 }
-*/
-func LoadAndParse(itemLoadTask priceloader.LoadTask) {
 
+func (pParser *ParserObject) LoadItems(pCategory *priceloader.Category) {
+	url := pParser.CustomParserActions.CreateItemsUrl(pCategory.URL)
+	result, err := webreader.DoRequest(url, pParser.Options)
+	if err == nil {
+		priceloader.PriceList.DeleteItemsCategory(pCategory)
+	}
+	log.CheckHtml(url, result, "debug")
+	pParser.CustomParserActions.ParseItems(pCategory, result)
+}
+
+func LoadAndParse(itemLoadTask priceloader.LoadTask) {
+	itemLoadTask.Handler(itemLoadTask.Pointer)
 }
